a80f112334c1197930a1d128b1f162abba50f060
EvanTomasek <Evan.Tomasek@s.capregboces.org>
2024-03-06 13:14:13 -0500

    done

--- exercise9/Program.cs
+++ exercise9/Program.cs
@@ -12,14 +12,31 @@
 
             //print rows going down
             Console.WriteLine();
-            for (int a =1; a < height; a++) 
+            for (int a = 1; a < height; a++)
             {
-               //print content of the row to the right
-               for (int b =1; b < width; b++) 
+                //print content of the row to the right
+                for (int b = 1; b < width; b++)
                 {
                     Console.Write("*");
                 }
-               Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            //problem  2
+
+            Console.WriteLine("Enter the MAX number of asterisks: ");
+            int asterisks = int.Parse(Console.ReadLine());
+
+            //print rows going down
+            Console.WriteLine();
+            for (int a = 1; a <= asterisks; a++)
+            {
+                //print content adding one more to the next row
+                for (int b = 1; b < a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
         }
     }
